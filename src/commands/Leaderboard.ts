058c93eb7973d2f24aa6d52437f9bb387423c7dc
raziman <raziman.terra@gmail.com>
2021-12-05 16:39:01 +0800

    add leaderboard command

---
+++ src/commands/Leaderboard.ts
@@ -0,0 +1,26 @@
+import { Command } from "@jiman24/commandment";
+import { Message, MessageEmbed } from "discord.js";
+import { client } from "../index";
+import { bold, currency } from "../utils";
+
+export default class extends Command {
+  name = "leaderboard";
+  aliases = ["l"];
+
+  exec(msg: Message) {
+
+    const player = client.players.array()
+      .sort((a, b) => b.shards - a.shards)
+      .map((x, i) => `${i + 1}. ${x.name} \`${x.shards}\``)
+      .slice(0, 10)
+      .join("\n");
+
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Leaderboard")
+      .setDescription(bold(`Name | ${currency}\n`) + player);
+
+    msg.channel.send({ embeds: [embed] });
+  }
+}
+
