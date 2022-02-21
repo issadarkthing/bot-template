986c554e6c2d595ae800fefd855f90e59402620e
raziman <raziman.terra@gmail.com>
2022-02-21 12:33:50 +0800

    use variable currency name in profile

--- src/structure/Player.ts
+++ src/structure/Player.ts
@@ -82,7 +82,7 @@ export class Player extends PlayerRPG {
 
     profile.spliceFields(armorIndex, 2);
 
-    profile.addField("Coins", code(this.coins), true);
+    profile.addField(currency, code(this.coins), true);
     profile.addField("Win", code(this.win), true);
     profile.addField("Hunt", code(this.hunt), true);
 
