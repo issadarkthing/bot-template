64629172dc40cb4175a81deab8a03762cba9e4f4
raziman <raziman.terra@gmail.com>
2022-02-25 22:54:37 +0800

    change pretext for create command

--- src/commands/Create.ts
+++ src/commands/Create.ts
@@ -26,7 +26,7 @@ export default class extends Command {
     msg.channel.send(
       `Use \`${prefix}profile\` to checkout your profile`
     )
-    msg.channel.send(`Use \`${prefix}hunt\` to start hunting monsters!`);
+    msg.channel.send(`Use \`${prefix}battle\` to start battle monsters!`);
     msg.channel.send(`Use \`${prefix}help\` to check out other commands!`);
   }
 }
