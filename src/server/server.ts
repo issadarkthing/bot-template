52ea3506372bea8a8d06c54bd2bfcc71ea47b355
raziman <raziman@tutanota.com>
2022-03-11 14:50:50 +0800

    add cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { router as playerRouter } from "./routes/player";
+import cors from "cors";
 
 const PORT = process.env.PORT  || 3000;
 
 const app = express();
 
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
