b62e63a2a565ac8657ed7b17b8a8dc68ffdaf746
raziman <raziman@tutanota.com>
2022-03-28 17:12:45 +0800

    remove testing that makes database inconsistent

--- src/server/tests/player.spec.ts
+++ src/server/tests/player.spec.ts
@@ -49,35 +49,3 @@ describe("GET /player/:id", () => {
   })
 
 })
-
-describe("PATCH /player/:id", () => {
-
-  it("update player", async () => {
-    const res = await request(app)
-      .get("/player/948769926288650301")
-      .set("Authorization", `Bot ${process.env.API_TOKEN}`)
-      .expect("Content-Type", /json/)
-      .expect(200);
-
-    const oldBalance = res.body.coins;
-
-    await request(app)
-      .patch("/player/948769926288650301")
-      .send({ coins: Math.floor(Math.random() * 10) })
-      .set("Authorization", `Bot ${process.env.API_TOKEN}`)
-      .expect("Content-Type", /json/)
-      .expect(200);
-
-    const res1 = await request(app)
-      .get("/player/948769926288650301")
-      .set("Authorization", `Bot ${process.env.API_TOKEN}`)
-      .expect("Content-Type", /json/)
-      .expect(200);
-
-    const newBalance = res1.body.coins;
-
-    expect(oldBalance).to.not.equal(newBalance);
-
-  })
-
-})
