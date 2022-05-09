b87505bf357827e89096d31785a542bdb2094671
raziman <raziman@tutanota.com>
2022-05-09 10:47:55 +0800

    fix leaderboard command error

--- src/commands/Leaderboard.ts
+++ src/commands/Leaderboard.ts
@@ -12,7 +12,7 @@ export default class extends Command {
   async exec(msg: Message) {
 
     //@ts-ignore
-    const player = (await client.players.getMany(client.players.all))
+    const player = (await client.players.values)
       .sort((a, b) => b.coins - a.coins)
       .map((x, i) => `${i + 1}. ${x.name} \`${x.coins}\``)
       .slice(0, 10)
