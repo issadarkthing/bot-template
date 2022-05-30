ebd58f4bfc1648233cbd4d76e1eab0c0c12a8d2b
raziman <raziman@tutanota.com>
2022-05-30 17:27:57 +0800

    use select menu for !boss command

--- src/commands/Boss.ts
+++ src/commands/Boss.ts
@@ -1,5 +1,5 @@
 import { Command } from "@jiman24/commandment";
-import { Message } from "discord.js";
+import { Message, MessageActionRow, MessageSelectMenu } from "discord.js";
 import { Boss } from "../structure/Boss";
 import { Player } from "../structure/Player";
 import { 
@@ -13,71 +13,81 @@ import {
 import { ButtonHandler } from "@jiman24/discordjs-button";
 import { Battle } from "@jiman24/discordjs-rpg";
 import { MessageEmbed } from "../structure/MessageEmbed";
+import { stripIndents } from "common-tags";
 
 export default class extends Command {
   name = "boss";
   description = "fight boss";
 
-  async exec(msg: Message, args: string[]) {
+  async exec(msg: Message) {
 
     const player = await Player.fromUser(msg.author);
     const boss = Boss.all;
-    
-    const [arg1] = args;
-    
-    if (arg1) {
+    const options = boss.map(x => ({ label: x.name, value: x.id }));
 
-      const index = parseInt(arg1) - 1;
-      validateNumber(index)
-      validateIndex(index, boss);
+    const selectMenu = new MessageSelectMenu()
+      .setCustomId("boss")
+      .setPlaceholder("Nothing selected")
+      .addOptions(options);
 
-      const selectedBoss = boss[index];
-      const menu = new ButtonHandler(msg, selectedBoss.show());
-
-      menu.addButton("battle", async () => {
-
-        const battle = new Battle(msg, random.shuffle([player, selectedBoss]));
-
-        const winner = await battle.run();
-
-        if (winner.id === player.id) {
-
-          const { drop, xpDrop } = selectedBoss;
-
-          const currLevel = player.level;
-          player.addXP(xpDrop);
-          player.coins += drop;
-          player.win++;
-
-          player.save();
-
-          const embed = new MessageEmbed(msg.author)
-            .appendDescription(`${player.name} has earned ${bold(drop)} ${currency}!`)
-            .appendDescription(`${player.name} has earned ${bold(xpDrop)} xp!`);
-
-          this.sendEmbed(msg, embed);
-
-          if (currLevel !== player.level) {
-            embed.setDescription(`${player.name} is now on level ${bold(player.level)}!`);
-            this.sendEmbed(msg, embed);
-          }
-        }
-      })
-
-      menu.addCloseButton();
-
-      await menu.run();
-
-      return;
-    }
+    const row = new MessageActionRow()
+      .addComponents(selectMenu);
 
     const bossList = toNList(boss.map(x => x.name));
+    const bossEmbed = new MessageEmbed(msg.author)
+      .setDescription(
+        stripIndents`Please select a boss:
+        ${bossList}
+        `
+      );
 
-    const embed = new MessageEmbed(msg.author)
-      .setColor("RED")
-      .setTitle("Boss")
-      .setDescription(bossList)
+    const message = await msg.channel.send({ embeds: [bossEmbed], components: [row] });
+    const respond = await message.awaitMessageComponent({ componentType: "SELECT_MENU" });
 
-    this.sendEmbed(msg, embed);
+    if (!respond.isSelectMenu()) return;
+
+    respond.reply(`Opening boss`);
+    respond.deleteReply();
+    message.delete();
+
+    const selectedBoss = boss.find(x => x.id === respond.values[0])!;
+    const menu = new ButtonHandler(msg, selectedBoss.show());
+
+    menu.addButton("battle", async () => {
+
+      const battle = new Battle(msg, random.shuffle([player, selectedBoss]));
+
+      const winner = await battle.run();
+
+      if (winner.id === player.id) {
+
+        const { drop, xpDrop } = selectedBoss;
+
+        const currLevel = player.level;
+        player.addXP(xpDrop);
+        player.coins += drop;
+        player.win++;
+
+        player.save();
+
+        const embed = new MessageEmbed(msg.author)
+          .appendDescription(`${player.name} has earned ${bold(drop)} ${currency}!`)
+          .appendDescription(`${player.name} has earned ${bold(xpDrop)} xp!`);
+
+        this.sendEmbed(msg, embed);
+
+        if (currLevel !== player.level) {
+          embed.setDescription(`${player.name} is now on level ${bold(player.level)}!`);
+          this.sendEmbed(msg, embed);
+        }
+      }
+    })
+
+    menu.addCloseButton();
+
+    await menu.run();
+
+
+
   }
 }
