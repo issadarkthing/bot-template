16f7fe5380be96c0aa57ddf49e915a0b70da4065
raziman <raziman@tutanota.com>
2022-06-07 18:50:19 +0800

    ignore monsters.json

--- src/template.ts
+++ src/template.ts
@@ -2,10 +2,10 @@ import { Armor } from "./structure/Armor";
 import { Weapon } from "./structure/Weapon";
 import { Pet } from "./structure/Pet";
 import { Skill } from "./structure/Skill";
-import { Item } from "./structure/Item";
 import { writeFileSync } from  "fs";
+import { Monster } from "./structure/Monster";
 
-function create(items: Item[], filename: string) {
+function create(items: any[], filename: string) {
   const data = JSON.stringify(items, null, 2);
   writeFileSync(filename, data);
 }
@@ -14,5 +14,6 @@ create(Armor.all, "armors.json");
 create(Weapon.all, "weapons.json");
 create(Skill.all, "skills.json");
 create(Pet.all, "pets.json");
+create(Monster.all, "monsters.json");
 
 console.log("done importing items data to json file");
